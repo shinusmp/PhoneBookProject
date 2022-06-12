ad5371635581fb5da59e5236c4a969e3af64cb2e
shinusmp <shrinivaspatil12@gmail.com>
2022-06-12 10:57:49 +0530

    PHONE-2 model class created

--- src/main/java/com/bikkadIT/model/Contact.java
+++ src/main/java/com/bikkadIT/model/Contact.java
@@ -1,5 +1,76 @@
 package com.bikkadIT.model;
 
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity
 public class Contact {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer contact_id;
+	
+	private String contact_Name;
+	
+	private String email_id;
+	
+	private String contact_number;
+	
+	@CreationTimestamp
+	private Date created_Date;
+	@UpdateTimestamp
+	private Date updated_date;
+	
+	
+	public Integer getContact_id() {
+		return contact_id;
+	}
+	public void setContact_id(Integer contact_id) {
+		this.contact_id = contact_id;
+	}
+	public String getContact_Name() {
+		return contact_Name;
+	}
+	public void setContact_Name(String contact_Name) {
+		this.contact_Name = contact_Name;
+	}
+	public String getEmail_id() {
+		return email_id;
+	}
+	public void setEmail_id(String email_id) {
+		this.email_id = email_id;
+	}
+	public String getContact_number() {
+		return contact_number;
+	}
+	public void setContact_number(String contact_number) {
+		this.contact_number = contact_number;
+	}
+	public Date getCreated_Date() {
+		return created_Date;
+	}
+	public void setCreated_Date(Date created_Date) {
+		this.created_Date = created_Date;
+	}
+	public Date getUpdated_date() {
+		return updated_date;
+	}
+	public void setUpdated_date(Date updated_date) {
+		this.updated_date = updated_date;
+	}
+	
+	
+	
+	
+	
+	
+	
 
 }
